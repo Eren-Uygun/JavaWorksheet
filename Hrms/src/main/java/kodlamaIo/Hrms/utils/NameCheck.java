ec6721e3ef2257965b71e8907fcd5b4a7ad6246f
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-05-17 18:59:29 +0300

    D6A3 , Utils

---
+++ Hrms/src/main/java/kodlamaIo/Hrms/utils/NameCheck.java
@@ -0,0 +1,17 @@
+package kodlamaIo.Hrms.utils;
+
+public class NameCheck {
+	
+	public static boolean nameCheck(String firstName,String lastName) {
+		
+		if (firstName.length()>=2 && !firstName.isEmpty() && lastName.length()>=2 &&!lastName.isEmpty()){
+			return true;
+		}
+		else {
+			System.out.println("Bilgilerinizi kontrol ediniz.Bilgileriniz en az 2 karakter olmalı ve boş olmamalıdır.");
+			return false;
+		}
+		
+	}
+
+}
